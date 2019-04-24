361ce3e8961411e8d6b4ee2ea97ab00cb2ffb545
Renan Tarouco da Fonseca <renantarouco@gmail.com>
2019-04-24 16:55:00 -0300

    function javascript que adiciona um elemento de season na tela.

--- static/js/dom.js
+++ static/js/dom.js
@@ -1,14 +1,66 @@
-class Season {
-    constructor(name, overview, posterPath) {
-        this.name = name;
-        this.overview = overview;
-        this.posterPath = posterPath;
-    }
+const env = {
+    API_BASE_URL: 'https://api.themoviedb.org/3',
+    IMAGE_BASE_URL: 'http://image.tmdb.org/t/p',
+    API_KEY: '7e41f8c308e8c9521fbd018d53440a05'
+};
+
+function addSeasonComponent(season, posterPosition) {
+    let seasonComponentHTML =
+    `<div class="season-container-poster-${posterPosition}">
+        <div class="season-title">
+            <h4>${season.name}</h4>
+        </div>
+        <div class="season-poster">
+            <img
+                src="${env.IMAGE_BASE_URL}/original${season.poster_path}"
+                alt="${season.name}'s poster.">
+        </div>
+        <div class="season-overview">
+            <p>
+                ${season.overview}
+            </p>
+            <p>
+                <strong>Episodios: </strong> ${season.episode_count}
+            </p>
+            <p>
+                <strong>Estreia: </strong> ${season.air_date}
+            </p>
+        </div>
+    </div>`;
+    let seasonComponent = document.createElement('div');
+    seasonComponent.innerHTML = seasonComponentHTML;
+
+    document.getElementById('seasons').appendChild(seasonComponent);
 }
 
-function addSeasonComponent(season) {
-    let seasonComponent = document.createElement('section');
-    seasonComponent.style.display = 'flex';
-    let seasonNameElement = document.createElement('div');
+/*function addSeasonComponent(season, posterPosition) {
+    let seasonComponent = document.createElement('div');
+    seasonComponent.classList.add(`season-container-poster-${posterPosition}`);
+
+    let seasonTitleElement = document.createElement('div');
+    seasonTitleElement.classList.add('season-title');
+    seasonTitleElement.appendChild(
+        document.createElement('h4').appendChild(
+            document.createTextNode(season.name)
+        )
+    );
+    seasonComponent.appendChild(seasonTitleElement);
+
     let seasonOverviewElement = document.createElement('div');
-}
+    seasonOverviewElement.classList.add('season-overview');
+    seasonOverviewElement.appendChild(
+        document.createElement('p').appendChild(
+            document.createTextNode(season.overview)
+        )
+    );
+    seasonComponent.appendChild(seasonOverviewElement);
+
+    let seasonPosterElement = document.createElement('div');
+    seasonPosterElement.classList.add('season-poster');
+    let posterImgElement = document.createElement('img');
+    posterImgElement.src = `${env.IMAGE_BASE_URL}/original${season.poster_path}`;
+    seasonPosterElement.appendChild(posterImgElement);
+    seasonComponent.appendChild(seasonPosterElement);
+
+    document.getElementById('seasons').appendChild(seasonComponent);
+}*/
